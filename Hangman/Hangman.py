57d1ca383d25fe0a6a41034b88f69289e16dc326
Harshita <91948987+harshitasaini469@users.noreply.github.com>
2022-07-25 22:47:44 +0530

    Update Hangman.py

--- Hangman/Hangman.py
+++ Hangman/Hangman.py
@@ -3,8 +3,10 @@ import string;
 from words import words;
 
 def get_valid_word(words):
+
     word=random.choice(words);
-    while '-' in word or ' ' in word:
+
+    while '-' in word or ' ' in word and len(word)>=2:
         word=random.choice(word);
 
     return word.upper();
@@ -12,40 +14,61 @@ def get_valid_word(words):
 def hangman():
     
     word=get_valid_word(words);
+
     word_letters=set(word); # letters in the word
+
+    letters=random.sample(word_letters,k=len(word)//3);
+
     alphabet=set(string.ascii_uppercase); # list of upper case letters
+
     used_letters=set(); # what the user has guessed
+
+    wordList=[]
+
+    for i in range(len(letters)):
+        used_letters.add(letters[i])
+
+    # print(used_letters);
 
     lives=7; # user have 7 lives to guess the correct letter
 
     #getting user input
-    while len(word_letters) > 0 and lives > 0:
+    while len(word_letters)-len(letters) > 0 and lives > 0:
 
         #what current word is (i.e W-R D);
         wordList=[letter if letter in used_letters else '-' for letter in word ];
         print('Current Word : ', ' '.join(wordList));
        
         user_letter=input('Guess a letter : ').upper();
-
-        if user_letter in alphabet-used_letters:   # adding the letter that is not used yet
+        
+        if user_letter in alphabet-used_letters:  # adding the letter that is not used yet
+            
             used_letters.add(user_letter);
+            
             if user_letter in word_letters:
+                
                 word_letters.remove(user_letter); # removing the correctly guessed letter from word_letters
+           
             else: 
+               
                 lives = lives - 1;
+                
                 print(f'{user_letter} letter not in the word, You have {lives} lives left\n')
+        
         elif user_letter in used_letters:
-            print('you have already guessed this character');
-        else : print('invalid character');
+           
+            print('you have already guessed this character\n');
+       
+        else : print('invalid character\n');
 
          # letters used
-        print(f'You have used these letters:',' '.join(used_letters),'\n');
-
-    
+        print(f'Already used letters :',' '.join(used_letters),'\n'); 
 
     if(lives==0):
-        print(f"Sorry, you died!\nThe word was \"{word}\"");
+        print(f"Sorry, you died!\nThe word was \"{word}\" ");
     else:
-        print(f"Yay! you guessed the word {word} correctly!!!");
+        print(f"Yay! you guessed the word \"{word}\" correctly!!!");  
+
+
 
 hangman()
